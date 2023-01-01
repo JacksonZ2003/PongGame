055e75eb1c149e44f826238de8886785d3552cad
JacksonZ2003 <68933473+JacksonZ2003@users.noreply.github.com>
2022-12-31 20:24:12 -0500

    Game progress

--- game.py
+++ game.py
@@ -1,6 +1,9 @@
 import pygame
 import sys 
 
+background = pygame.Surface((600,600))
+background.fill("Black")
+
 class Game:
     def __init__(self):
         pygame.init()
@@ -15,14 +18,30 @@ class Game:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     exit()
+                if event.type == pygame.KEYDOWN:
+                    self.screen.blit(background,(0,0))
+                    player1.move(self.screen, event)
+                    player2.move(self.screen, event)
             
             pygame.display.update()
 
 class Player:
     def __init__(self, screen, x, y, up, down):
-        player = pygame.Surface((10,100))
-        player.fill("White")
-        screen.blit(player, (x,y))
+        self.x=x
+        self.y=y
+        self.up=up
+        self.down=down
+        self.player = pygame.Surface((10,100))
+        self.player.fill("White")
+        screen.blit(self.player,(x,y))
+    
+    def move(self, screen, event): 
+        if event.key == pygame.K_UP:
+            self.y-=10
+            screen.blit(self.player, (self.x,self.y))
+        elif event.key == pygame.K_DOWN:
+            self.y+=10
+            screen.blit(self.player, (self.x,self.y))
 
 
 if __name__ == "__main__":
