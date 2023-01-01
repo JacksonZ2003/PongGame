8bbddf0a8294ef32ab7380aa7ceb84d822a31018
JacksonZ2003 <68933473+JacksonZ2003@users.noreply.github.com>
2023-01-01 14:01:54 -0500

    Update

--- game.py
+++ game.py
@@ -11,6 +11,10 @@ class Game:
         pygame.display.set_caption("Game")
 
     def runGame(self):
+        p1move_up = False
+        p1move_down = False
+        p2move_up = False
+        p2move_down = False
         player1 = Player(self.screen,0,250,"w","s")
         player2 = Player(self.screen,590,250,"up","down")
         while True:
@@ -19,9 +23,27 @@ class Game:
                     pygame.quit()
                     exit()
                 if event.type == pygame.KEYDOWN:
-                    self.screen.blit(background,(0,0))
-                    player1.move(self.screen, event)
-                    player2.move(self.screen, event)
+                    if (event.key == pygame.K_UP):
+                        p2move_up=True
+                    if (event.key == pygame.K_DOWN):
+                        p2move_down = True
+                    if (event.key == pygame.K_w):
+                        p1move_up=True
+                    if (event.key == pygame.K_s):
+                        p1move_down = True
+                elif event.type == pygame.KEYUP:
+                    if (event.key == pygame.K_UP):
+                        p2move_up=False
+                    if (event.key == pygame.K_DOWN):
+                        p2move_down=False
+                    if (event.key == pygame.K_w):
+                        p1move_up=False
+                    if (event.key == pygame.K_s):
+                        p1move_down = False
+                
+                self.screen.blit(background,(0,0))
+                player1.move(p1move_up, p1move_down, p2move_up, p2move_down, self.screen)
+                player2.move(p1move_up, p1move_down, p2move_up, p2move_down, self.screen)
             
             pygame.display.update()
 
@@ -35,17 +57,17 @@ class Player:
         self.player.fill("White")
         screen.blit(self.player,(x,y))
     
-    def move(self, screen, event): 
-        if (event.key == pygame.K_UP) and (self.y > 0) and (self.up=="up"):
+    def move(self,p1up, p1down, p2up, p2down, screen): 
+        if (p2up) and (self.y > 0) and (self.up=="up"):
             self.y-=10
             screen.blit(self.player, (self.x,self.y))
-        elif (event.key == pygame.K_w) and (self.y > 0) and (self.up=="w"):
+        elif (p1up) and (self.y > 0) and (self.up=="w"):
             self.y-=10
             screen.blit(self.player, (self.x,self.y))
-        elif (event.key == pygame.K_DOWN) and (self.y < 500) and (self.down == "down"):
+        elif (p2down) and (self.y < 500) and (self.down == "down"):
             self.y+=10
             screen.blit(self.player, (self.x,self.y))
-        elif (event.key == pygame.K_s) and (self.y < 500) and (self.down == "s"):
+        elif (p1down) and (self.y < 500) and (self.down == "s"):
             self.y+=10
             screen.blit(self.player, (self.x,self.y))
         else:
